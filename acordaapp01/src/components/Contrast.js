1708f59646a1a6f28a14fe5d0e9f865b26b2693c
RonjayAcorda <20214060@s.ubaguio.edu>
2023-06-21 11:06:37 +0800

    6/21/23

--- acordaapp01/src/components/Contrast.js
+++ acordaapp01/src/components/Contrast.js
@@ -3,14 +3,30 @@ import React, { useState } from 'react';
 function Contrast() {
   const [isToggled, setIsToggled] = useState(false);
 
-  const handleToggle = () => {
+  const darkmode = () => {
     setIsToggled(!isToggled);
     document.body.style.backgroundColor = isToggled ? 'white' : 'black';
   };
+  const yellowmode = () => {
+    setIsToggled(!isToggled);
+    document.body.style.backgroundColor = isToggled ? 'white' : 'yellow';
+  };
+  const greenmode = () => {
+    setIsToggled(!isToggled);
+    document.body.style.backgroundColor = isToggled ? 'white' : 'green';
+  };
+  const bluemode = () => {
+    setIsToggled(!isToggled);
+    document.body.style.backgroundColor = isToggled ? 'white' : 'blue';
+  };
+
 
   return (
     <div>
-      <button onClick={handleToggle}>Make it Black</button>
+      <button onClick={darkmode}>Dark Mode</button>
+      <button onClick={yellowmode}>Yellow Mode</button>
+      <button onClick={greenmode}>Green Mode</button>
+      <button onClick={bluemode}>Blue Mode</button>
     </div>
   );
 }
